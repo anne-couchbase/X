ea213bcaeda2861938586ec0ce87f12358f72ecc
Cloud User <ec2-user@ip-172-31-9-200.us-west-1.compute.internal>
2019-09-15 02:02:54 +0000

    funstuff Single Instance

---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "security_group_id" { default = "sg-07f933152a663a689" }
+
+variable "key_name" { default = "funstuff" }
+variable "public_key" { default = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEArY9HEem/aIFZ/8OF8SgcRKDjjAyvThHXzxZwOjipx/EubMrnbgV4XP2fGt/EVVhOIDnHYVTj/bVWumoj99D8l1tYhv0/CP2Y68DB2FrqeQE1UTi/dWZPMeHk0POy17HMESyYGfB32lvRAe5jeijW66lFx3V9jau06/1cMnvfz3YPoh//IgEi64kBOS9EUuwTjT4wx2HvD+sUty3ScWivPELYOKpBhXe0nwybouPDDL8PwZu3ZqnW2oKBzq4mp5xmhKIHvbdsKOjG4r2unb+Yx8HJqaDrf3XrBIusW86TdN8gKxvGOUoN2s4Yc1o44zwiFX9XduSWnf+5JgszcGZqvQIDAQAB" }
